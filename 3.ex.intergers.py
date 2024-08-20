a65fccc2682182a24332fe068697c29f0b3b0f01
Ergashboyev Abdulloh <ergashboyevabdulloh0@gmail.com>
2024-08-20 18:11:11 +0800

    Integers
    Working with various numbers during programming is natural. In Python, there are several types of numbers. Let's get to know them more closely.

---
+++ 3.ex.intergers.py
@@ -0,0 +1,32 @@
+
+"""What diffenrce between  name and age, type - identify the code type"""
+name = 'Jobir'
+age = 36
+print(type(name))  # ism degan o'zgaruvchining  turini konsolga chiqaramiz
+print(type(age))  #ism yosh degan o'zgaruvchining turini konsolga chiqaramiz
+
+
+
+"""A program that consoles the square and cube of a number entered by the user"""
+kvadrat_hisobla=int(input("Enter any number: "))
+kvadrat= kvadrat_hisobla **2
+print(f"{kvadrat_hisobla} ning {kvadrat} ga teng")
+
+
+
+
+"""A console program that asks for the user's age and calculates his year of birth"""
+age = int(input("Enter your age: "))
+calculate= 2024 - age
+print(f"Your years of birth {calculate}'s")
+
+
+
+"""A program that asks the user to enter two numbers and produces the sum, subtraction, multiplication, and division of the entered numbers"""
+first_n=float(int(input("Enter number: ")))
+second_n=float(int(input("Enter number: ")))
+print(f"{first_n}+{second_n} =",first_n+second_n)
+print(f"{first_n}-{second_n} =",first_n-second_n)
+print(f"{first_n}*{second_n} =", first_n*second_n)
+print(f"{first_n}/{second_n} =", first_n/second_n)
+
