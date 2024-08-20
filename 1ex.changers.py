610a39a364af574286bdcbf233d440ed654c3987
Ergashboyev Abdulloh <ergashboyevabdulloh0@gmail.com>
2024-08-20 17:48:09 +0800

    introduction to Pyhton

---
+++ 1ex.changers.py
@@ -0,0 +1,43 @@
+
+"""Hello Python"""
+
+# enter_python= "Hello World!"
+# print(enter_python)
+
+
+# """Give a name and age by arrange"""
+# name= "John"
+# age = 19
+# print(name)
+# print(age)
+
+
+# """Using one 'ism' can give multiple words and numbers"""
+# ism="Muhammad"
+# print(ism)
+# ism = "Abdulloh"
+# print(ism)
+
+
+# """'message' can give words and numbers"""
+# message="Welcome to my Pyhton Project"
+# print(message)
+# message=2024
+# print(message)
+
+
+
+# """Don't use Python Keyword"""
+# class = "Love pyhton"
+# print(class)
+
+
+
+# """Using '*'symbol and can calculate python format """
+# radius = 5
+# pi = 3.14159
+# calculate = pi * radius**2
+# print("Radius" , radius, "face of a circle equal to=", calculate)
+
+
+
